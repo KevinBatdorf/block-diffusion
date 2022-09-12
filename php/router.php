54f104c9eb79775c55d61aae3d78eb85c5f5d659
Kevin Batdorf <kbat82@gmail.com>
2022-09-12 02:03:46 -0400

    Add class check

--- php/router.php
+++ php/router.php
@@ -1,42 +1,44 @@
 <?php
 defined( 'ABSPATH' ) or die;
 
-class KBSDRouter extends \WP_REST_Controller
-{
-    protected static $instance = null;
-    public function getHandler($namespace, $endpoint, $callback) {
-        \register_rest_route(
-            $namespace,
-            $endpoint,
-            [
-                'methods' => 'GET',
-                'callback' => $callback,
-                'permission_callback' => function() {
-                    return \current_user_can('upload_files');
-                },
-            ]
-        );
-    }
-    public function postHandler($namespace, $endpoint, $callback) {
-        \register_rest_route(
-            $namespace,
-            $endpoint,
-            [
-                'methods' => 'POST',
-                'callback' => $callback,
-                'permission_callback' => function() {
-                    return \current_user_can('upload_files');
-                },
-            ]
-        );
-    }
-
-    public static function __callStatic($name, array $arguments) {
-        $name = "{$name}Handler";
-        if (is_null(self::$instance)) {
-            self::$instance = new static();
+if (!class_exists('KBSDRouter')) {
+    class KBSDRouter extends \WP_REST_Controller
+    {
+        protected static $instance = null;
+        public function getHandler($namespace, $endpoint, $callback) {
+            \register_rest_route(
+                $namespace,
+                $endpoint,
+                [
+                    'methods' => 'GET',
+                    'callback' => $callback,
+                    'permission_callback' => function() {
+                        return \current_user_can('upload_files');
+                    },
+                ]
+            );
         }
-        $r = self::$instance;
-        return $r->$name('kevinbatdorf/stable-diffusion', ...$arguments);
+        public function postHandler($namespace, $endpoint, $callback) {
+            \register_rest_route(
+                $namespace,
+                $endpoint,
+                [
+                    'methods' => 'POST',
+                    'callback' => $callback,
+                    'permission_callback' => function() {
+                        return \current_user_can('upload_files');
+                    },
+                ]
+            );
+        }
+
+        public static function __callStatic($name, array $arguments) {
+            $name = "{$name}Handler";
+            if (is_null(self::$instance)) {
+                self::$instance = new static();
+            }
+            $r = self::$instance;
+            return $r->$name('kevinbatdorf/stable-diffusion', ...$arguments);
+        }
     }
 }
