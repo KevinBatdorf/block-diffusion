778374f42c43851fd55308099b0c75df83aa8922
Kevin Batdorf <kbat82@gmail.com>
2022-09-12 14:02:40 -0400

    Update select case

--- src/models.ts
+++ src/models.ts
@@ -4,7 +4,7 @@ import { ModelAttributes } from './types';
 export const models: ModelAttributes[] = [
     {
         id: 'stability-ai/stable-diffusion',
-        name: __('AI Prompt To Image', 'stable-diffusion'),
+        name: __('Prompt to Image', 'stable-diffusion'),
         description: __(
             'A latent text-to-image diffusion model capable of generating photo-realistic images given any text input',
             'stable-diffusion',
@@ -14,7 +14,7 @@ export const models: ModelAttributes[] = [
     },
     {
         id: 'tencentarc/gfpgan',
-        name: __('Reverse Image To Prompt', 'stable-diffusion'),
+        name: __('Reverse Image to Prompt', 'stable-diffusion'),
         description: __(
             'Get an approximate text prompt, with style, matching an image that you provide',
             'stable-diffusion',
@@ -34,7 +34,7 @@ export const models: ModelAttributes[] = [
     },
     {
         id: 'deforum/deforum_stable_diffusion',
-        name: __('AI Prompt Animation', 'stable-diffusion'),
+        name: __('Prompt Animation', 'stable-diffusion'),
         description: __(
             'Animating prompts with stable diffusion',
             'stable-diffusion',
